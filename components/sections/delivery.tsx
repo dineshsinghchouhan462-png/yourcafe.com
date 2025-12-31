faf3cf2d5a62880148b003c11e3d073228f23f3d
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2025-12-31 14:47:07 +0530

    Create delivery.tsx

---
+++ components/sections/delivery.tsx
@@ -0,0 +1,7 @@
+export default function Delivery() {
+  return (
+    <section className="py-16 bg-gray-50 text-center">
+      <p>Available on Zomato & partner platforms</p>
+    </section>
+  );
+}
