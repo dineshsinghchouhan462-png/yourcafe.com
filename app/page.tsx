67bd553a1fcf134f45592f5a0c32e9091a0eaa89
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2025-12-31 15:07:40 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "@/components/sections/hero";
-import About from "@/components/sections/about";
-import Highlights from "@/components/sections/highlights";
-import Gallery from "@/components/sections/gallery";
-import Delivery from "@/components/sections/delivery";
-import Contact from "@/components/sections/contact";
+import Hero from "../components/sections/hero";
+import About from "../components/sections/about";
+import Highlights from "../components/sections/highlights";
+import Gallery from "../components/sections/gallery";
+import Delivery from "../components/sections/delivery";
+import Contact from "../components/sections/contact";
 
 export default function Home() {
   return (
