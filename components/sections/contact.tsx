7d3e3762ebdbfabb63fcfe3041f0770c7874cdbb
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2025-12-31 14:47:36 +0530

    Create contact.tsx

---
+++ components/sections/contact.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <section id="contact" className="py-20 text-center">
+      <p>📞 088906 67769</p>
+      <p>📧 thelazybarncafe@gmail.com</p>
+    </section>
+  );
+}
