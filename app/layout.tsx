1193d6781a7fb05d10f67d72ade891f21573cd5a
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2025-12-31 16:17:58 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
 import "./globals.css";
-import Navigation from "../components/navigation";
-import Footer from "../components/footer";
-
-export const metadata = {
-  title: "The Lazy Barn Cafe | Jodhpur",
-  description: "The Lazy Barn Cafe – food, coffee & calm vibes in Jodhpur",
-};
+import Navigation from "@/components/navigation";
+import Footer from "@/components/footer";
 
 export default function RootLayout({
   children,
