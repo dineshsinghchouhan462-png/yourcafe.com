69b6df1cca342d747154571469b90590ec113f7e
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2026-01-01 20:26:38 +0530

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,7 +3,7 @@
 export default function Navigation() {
   return (
     <header className="absolute top-0 left-0 w-full z-30 pointer-events-none">
-      <div className="flex items-center justify-between px-6 pt-6 md:px-16 md:pt-12 pointer-events-auto">
+      <div className="flex items-center justify-between px-6 pt-10 md:px-16 md:pt-12 pointer-events-auto">
         
         {/* Logo */}
         <div className="font-serif text-[17px] md:text-[20px] tracking-[0.04em] font-normal text-[#F4EFE9]">
