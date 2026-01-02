513455209e9af226514272262470db7ed74b4f4e
dineshsinghchouhan462-png <dineshsinghchouhan462@gmail.com>
2026-01-02 15:06:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "../components/sections/hero";
+import IntroSection from "../components/sections/IntroSection";
 import About from "../components/sections/about";
 import Highlights from "../components/sections/highlights";
 import Gallery from "../components/sections/gallery";
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <IntroSection />
       <About />
       <Highlights />
       <Gallery />
